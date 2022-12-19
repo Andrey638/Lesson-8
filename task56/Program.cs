186cf0a6d9e284618de3c6ebfb5fa5769c62802e
Andrey3544 <box4435@mail.ru>
2022-12-19 23:11:07 +0300

    task56

---
+++ task56/Program.cs
@@ -0,0 +1,33 @@
+﻿//Задайте прямоугольный двумерный массив. Напишите программу, 
+//которая будет находить строку с наименьшей суммой элементов.
+Console.WriteLine("Введите размер массива m - n");
+int m = Convert.ToInt32(Console.ReadLine());
+int n = Convert.ToInt32(Console.ReadLine());
+int[,] array = new int[m, n];
+Random random = new Random();
+int minsum = Int32.MaxValue;
+int index = 0;
+Console.WriteLine();
+for (int i = 0; i < m; i++)
+{
+    for (int j = 0; j < n; j++)
+    {
+        array[i, j] = random.Next(1, 10);
+        Console.Write(array[i, j] + " ");
+    }
+    Console.WriteLine();
+}
+for (int i = 0; i < m; i++)
+{
+    int sum = 0;
+    for (int j = 0; j < n; j++)
+    {
+        sum = sum + array[i, j];        
+    }
+    if (sum < minsum)
+    {
+        minsum = sum;
+        index++;
+    }
+}
+Console.WriteLine("строка с наименьшей суммой елементов : " + (index) + ", сумма равна: " + (minsum));
